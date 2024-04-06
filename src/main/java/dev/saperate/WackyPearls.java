dbc16943969a980a03464d186333e7ab4e7299b3
saperate <61527642+saperate@users.noreply.github.com>
2024-04-06 11:06:58 -0400

    Fixed bug where you couldn't open a server

--- src/main/java/dev/saperate/WackyPearls.java
+++ src/main/java/dev/saperate/WackyPearls.java
@@ -5,12 +5,14 @@ import dev.saperate.item.LoversPearl;
 import dev.saperate.item.PhantomPearl;
 import dev.saperate.item.RedstonePearl;
 import dev.saperate.item.SlimePearl;
+import net.fabricmc.api.EnvType;
 import net.fabricmc.api.ModInitializer;
 
 import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricEntityTypeBuilder;
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.block.DispenserBlock;
 import net.minecraft.entity.EntityDimensions;
 import net.minecraft.entity.EntityType;
@@ -73,10 +75,12 @@ public class WackyPearls implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		EntityRendererRegistry.register(SLIMEPEARL, SlimePearlEntityRenderer::new);
-		EntityRendererRegistry.register(REDSTONEPEARL, RedstonePearlEntityRenderer::new);
-		EntityRendererRegistry.register(PHANTOMPEARL, PhantomPearlEntityRenderer::new);
-		EntityRendererRegistry.register(LOVERSPEARL, LoversPearlEntityRenderer::new);
+		if(FabricLoader.getInstance().getEnvironmentType().equals(EnvType.CLIENT)) {
+			EntityRendererRegistry.register(SLIMEPEARL, SlimePearlEntityRenderer::new);
+			EntityRendererRegistry.register(REDSTONEPEARL, RedstonePearlEntityRenderer::new);
+			EntityRendererRegistry.register(PHANTOMPEARL, PhantomPearlEntityRenderer::new);
+			EntityRendererRegistry.register(LOVERSPEARL, LoversPearlEntityRenderer::new);
+		}
 		DispenserBlock.registerBehavior(LOVERSPEARLITEM,LOVERSPEARLITEM);
 
 		ItemGroupEvents.modifyEntriesEvent(ItemGroups.TOOLS).register(content -> {
